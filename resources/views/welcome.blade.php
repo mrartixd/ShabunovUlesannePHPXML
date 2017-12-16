151804e27d50f95c2eb8c1795760605f03123a29
Artur <artur.shabunov@gmail.com>
2017-12-16 16:32:34 +0200

    changes welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts.app')
 @section('title','Новости')
 @section('maincontent')
-<div class="card" style="width: 20rem;">
+<?php $news =\DB::select('select * from news'); ?>
+  @foreach ($news as $new)
+  <div class="card" style="width: 20rem;">
   <div class="card-body">
-    <h4 class="card-title">Title</h4>
-    <h6 class="card-subtitle mb-2 text-muted">Date and category</h6>
-    <p class="card-text">Description</p>
-    <a href="#" class="card-link">link</a>
+    <h4 class="card-title">{{$new->title}}</h4>
+    <h6 class="card-subtitle mb-2 text-muted">{{$new->pubdate}} &nbsp; {{$new->id_category}}</h6>
+    <p class="card-text">{{$new->description}}</p>
+    <a href="{{$new->link}}" class="card-link">{{$new->link}}</a>
   </div>
 </div>
+@endforeach
+@empty($news)
+<div class="alert alert-danger" role="alert">
+  <h4 class="alert-heading">Нету Новостей!</h4>
+  <p>Зарегистрируйтесь и добавьте первую запись</p>
+  <hr>
+  <p class="mb-0">Или войдите и добавьте первую запись</p>
+</div>
+@endempty
 @endsection
--- storage/framework/views/f1b250727d862d45b4ebe1b1ecc8b610728a7149.php
+++ storage/framework/views/f1b250727d862d45b4ebe1b1ecc8b610728a7149.php
@@ -1,12 +1,23 @@
 <?php $__env->startSection('title','Новости'); ?>
 <?php $__env->startSection('maincontent'); ?>
-<div class="card" style="width: 20rem;">
+<?php $news =\DB::select('select * from news'); ?>
+  <?php $__currentLoopData = $news; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $new): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+  <div class="card" style="width: 20rem;">
   <div class="card-body">
-    <h4 class="card-title">Title</h4>
-    <h6 class="card-subtitle mb-2 text-muted">Date and category</h6>
-    <p class="card-text">Description</p>
-    <a href="#" class="card-link">link</a>
+    <h4 class="card-title"><?php echo e($new->title); ?></h4>
+    <h6 class="card-subtitle mb-2 text-muted"><?php echo e($new->pubdate); ?> &nbsp; <?php echo e($new->id_category); ?></h6>
+    <p class="card-text"><?php echo e($new->description); ?></p>
+    <a href="<?php echo e($new->link); ?>" class="card-link"><?php echo e($new->link); ?></a>
   </div>
 </div>
+<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+<?php if(empty($news)): ?>
+<div class="alert alert-danger" role="alert">
+  <h4 class="alert-heading">Нету Новостей!</h4>
+  <p>Зарегистрируйтесь и добавьте первую запись</p>
+  <hr>
+  <p class="mb-0">Или войдите и добавьте первую запись</p>
+</div>
+<?php endif; ?>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
